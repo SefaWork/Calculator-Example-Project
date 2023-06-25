a76161f94ad8ac9f822afb62563fc6bc8104a114
SefaWork <sefa.toksoy.2@gmail.com>
2023-06-25 14:17:20 +0300

    Calculator class implemented.

---
+++ calculator.cpp
@@ -0,0 +1,54 @@
+#include "calculator.h"
+#include <string>
+
+void Calculator::run() {
+    screen.output("Starting calculator...");
+
+    while(true) {
+        promptOperation();
+        
+        if(operation == 0) break;
+        else if(operation > 4 || operation < 0) screen.output("Invalid operation.");
+        else {
+            promptNumbers();
+
+            switch (operation)
+            {
+                case 1:
+                    int additionResult = num1 + num2;
+                    screen.output("The result is " + std::to_string(additionResult));
+                    break;
+                case 2:
+                    int additionResult = num1 + num2;
+                    screen.output("The result is " + std::to_string(additionResult));
+                    break;
+                case 3:
+                    int multiplicationResult = num1 * num2;
+                    screen.output("The result is " + std::to_string(multiplicationResult));
+                    break;
+                case 4:
+                    double divisionResult = num1 / num2;
+                    screen.output("The result is " + std::to_string(divisionResult));
+                    break;
+            }
+        }
+    }
+
+    screen.output("Shutting down calculator...");
+}
+
+void Calculator::promptOperation() {
+    screen.output("\n\nSelect an operation:\n\t0- CANCEL\n1- ADD (+)\t\t2- SUB (-)\n3- MULT (x)\t\t4- DIV (/)\n\nPlease make a selection(1-4): ");
+    operation = keypad.getInput();
+    screen.output("\n");
+}
+
+void Calculator::promptNumbers() {
+    screen.output("\nEnter the first number: ");
+    num1 = keypad.getInput();
+
+    screen.output("\nEnter the second number: ");
+    num2 = keypad.getInput();
+
+    screen.output("\n");
+}
--- calculator.h
+++ calculator.h
@@ -1,18 +1,22 @@
 #ifndef CALCULATOR_H
 #define CALCULATOR_H
 
+#include "screen.h"
+#include "keypad.h"
+
 class Calculator {
 	public:
 		void run();
 		
 		void promptOperation();
 		void promptNumbers();
-		void doCalculation(char, int, int);
-		char getOperationSign(int);
 	private:
 		int operation;
 		int num1;
 		int num2;
+
+		Screen screen;
+		Keypad keypad;
 };
 
 #endif
